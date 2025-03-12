7dac8aa65b33264144fe567bf4feafa17b1b806f
dtnorth <derektnorthcote@gmail.com>
2025-03-12 15:25:15 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,25 @@
 variable "aws_account_id" {
   description = "AWS Account ID"
   type        = string
-  default     = "12345678912"
+  default     = "12345678912"                                # Modify as needed
 }
 
 variable "trusted_ip_range" {
   description = "Trusted IP range for security group"
   type        = string
-  default     = "192.168.1.0/24"
+  default     = "192.168.1.0/24"                             # Modify as needed
 }
 
 variable "aws_region" {
   description = "AWS Region"
   type        = string
-  default     = "eu-west-1"
+  default     = "eu-west-1"                                  # Modify as needed
 }
+
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]   # Modify as needed
+}
+
+
