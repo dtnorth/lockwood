9b4e9ebb3d86b103e74c5cc411ce02f0b267f9f3
dtnorth <derektnorthcote@gmail.com>
2025-03-12 10:26:40 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "trusted_ip_range" {
   type        = string
   default     = "192.168.1.0/24"
 }
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-west-1"
+}
