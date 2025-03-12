e569e030d4709ac407157403445dd5366b0f9633
dtnorth <derektnorthcote@gmail.com>
2025-03-12 09:28:12 +0000

    Define tfstate bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,28 @@ terraform {
     key            = "terraform/state.tfstate"
     region         = "eu-west-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"  # Optional for state locking
+    use_lockfile   = true
   }
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "flask-terraform-state-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
