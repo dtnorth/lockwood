e02ea92f845a6d24f5a5c979040818b5983a16c9
dtnorth <derektnorthcote@gmail.com>
2025-03-12 15:39:14 +0000

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,9 +3,9 @@ module "vpc" {
   name   = "flask-app-vpc"
   cidr   = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = var.aws_availability_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
