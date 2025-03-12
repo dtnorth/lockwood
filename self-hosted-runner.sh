37fea036a3e8691c9b0073a90917ff4b803467b5
dtnorth <derektnorthcote@gmail.com>
2025-03-12 15:49:12 +0000

    Update self-hosted-runner.sh

--- self-hosted-runner.sh
+++ self-hosted-runner.sh
@@ -3,7 +3,7 @@
 # Run self-hosted actions runner Linux x64
 #
 # Create the runner and start the configuration experience
-# ./config.sh --url https://github.com/dtnorth/lockwood --token ADFANXDUHX5WI3LMWXBYWQTH2FRHK
+# ./config.sh --url https://github.com/dtnorth/lockwood --token <redacted>
 #
 # Use this YAML in your workflow file for each job
 #   
